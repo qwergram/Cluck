8b7abd82b5b8dfb184e6629061288105b228a284
Norton Pengra <npengra317@gmail.com>
2016-03-26 00:49:38 -0700

    Update models.py

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+
+
+class Device(models.Model):
+    device_id = models.CharField(max_length=128)
+    device_name = models.CharField(max_length=128)
+
+
+class Menu(models.Model):
+    item_id = models.CharField(max_length=16)
+    option_id = models.CharField(max_length=16)
+
+
+class Log(models.Model):
+    log_type = models.CharField(max_length=10)
+    currenttime = models.DateTimeField()
+    elapsedseconds = models.CharField(max_length=10)
+    uxtimeremaining = models.CharField(max_length=10)
+    orbactive = models.CharField(max_length=5)
+    temp = models.FloatField()
+    tempslope60 = models.IntegerField()
+    currentorbits = models.FloatField()
+    percentagecomplete = models.FloatField()
+    estimatedremainingtime = models.CharField(max_length=10)
+    deviceremainingtime = models.CharField(max_length=10)
+    devicesynctime = models.CharField(max_length=10)
+    devicetemp = models.FloatField()
+    devicetempsynctime = models.IntegerField()
+    deviceorbits = models.FloatField()
+    deviceorbitssynctime = models.IntegerField()
+    deviceslope60 = models.IntegerField()
+    deviceslope60synctime = models.IntegerField()
