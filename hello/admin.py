f6a6d4fe7f8ae2611e5c9d00930150838f33e972
Norton Pengra <npengra317@gmail.com>
2016-03-26 00:53:27 -0700

    Register models with admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Device, Menu, Log
 
 # Register your models here.
+
+admin.site.register(Device)
+admin.site.register(Menu)
+admin.site.register(Log)
